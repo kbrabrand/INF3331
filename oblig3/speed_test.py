980b0bd9722f35ff1b2c3a4bdce78f43cb9c1139
Kristoffer Brabrand <kristoffer@brabrand.no>
2014-11-06 21:55:10 +0100

    Implemented timing of python vs. numpy/weave

---
+++ oblig3/speed_test.py
@@ -0,0 +1,33 @@
+from timeit import timeit;
+
+# Set up paths
+scripts   = ['denoise_numpy_weave', 'denoise_python'];
+test_file = 'assets/disasterbefore.jpg';
+out_file  = 'tmp/out.jpg';
+
+# Set up test params
+kappa 	   = 0.1;
+iterations = 5;
+executions = 10;
+
+print "Denoising %s %d times, \nwith kappa=%f and %d iterations \
+per execution.\n" % (test_file, executions, kappa, iterations);
+
+print "(execution times shown are totals)\n";
+
+# Get length of longest script name
+script_name_length = len(max(scripts, key=len));
+
+# Perform the test once for each script
+for script in scripts:
+	setup = "from %s import denoise_file" % script;
+	codeline = "denoise_file(\"%s\", \"%s\", %f, %d)" % (test_file, out_file, kappa, iterations);
+
+	# Prepare padded script name, for prettier output
+	padded_script_name = script.ljust(script_name_length);
+
+	# Perform timing of script
+	execution_time 	   = timeit(codeline, setup, number=executions);
+
+	# Output result
+	print "%s: %f seconds" % (padded_script_name, execution_time);
